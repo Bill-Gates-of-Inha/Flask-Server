0b2639e01610066106bff1b7fd11fcfa7bf81801
sukstar76 <sukstar76@gmail.com>
2021-05-25 21:36:34 +0900

    지용아 봐라

--- app.py
+++ app.py
@@ -10,17 +10,22 @@ app = Flask(__name__)
 def hello_jiyong():
     return 'hello_jiyong'
 
-
+##메소드는 post 로 사진을 body에 담을꺼임
 @app.route('/predict', methods=['POST'])
 def predict():
+    ###body에 담긴 이미지를 가져오는것 multi-part/form-data
     file = request.files['file']
+    ###file 이 FileStorage 타입이라 PIL 타입으로 변경해야함 그리구 리사이징도 함께
     image = Image.open(file)
     image = image.resize((224, 224))
+
+    ### 이 밑에는 수피가 알려준거
     img_batch = np.expand_dims(image, axis=0)
     img_preprocessed = tf.keras.applications.efficientnet.preprocess_input(img_batch)
     prediction = model.predict(img_preprocessed)
     idx = prediction[0].argmax()
 
+    ### 결과값을 정의해서 리턴하면 댐 난 그냥 짜장면 나오게 햇삼
     return labels[idx]
 
     """return jsonify(
@@ -31,6 +36,8 @@ def predict():
 
 
 if __name__ == '__main__':
+    ### 모델 로드하는거
     model = load_model('./model/Food_Classifier.h5')
+    ### 레이블화 된거를 list로 불러오기
     labels = open("./model/labels.txt", 'r').read().split('\n')
     app.run(debug=True)
